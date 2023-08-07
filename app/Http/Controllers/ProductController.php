7ba786cb6d9a71efba823a9fb3a164e3e810b478
Edzel <dev.edzeldacanay@gmail.com>
2023-08-07 16:41:51 +0800

    updated stripe checkout

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use App\Models\Product;
-use Exception;
 use http\Env\Response;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -14,49 +13,50 @@ class ProductController extends Controller
     public function index(Request $request)
     {
         $products = Product::all();
+
         return view('product.index', compact('products'));
     }
 
     public function checkout()
     {
-        \Stripe\Stripe::setApiKey('sk_test_51NcJkhFJK6oGjToujz6HzEokn7KwX4SPb2XPytLXjnrTDoZxbrofsOZmcjfgkh1mAnGmSiuLMaJgByYAW1okEgEj00rjXzjmRl');
+        \Stripe\Stripe::setApiKey("sk_test_51NcJkhFJK6oGjToujz6HzEokn7KwX4SPb2XPytLXjnrTDoZxbrofsOZmcjfgkh1mAnGmSiuLMaJgByYAW1okEgEj00rjXzjmRl");
 
         $products = Product::all();
         $lineItems = [];
         $totalPrice = 0;
-        foreach ($products as $product){
+        foreach ($products as $product) {
             $totalPrice += $product->price;
             $lineItems[] = [
-                    'price_data' => [
-                      'currency' => 'usd',
-                      'product_data' => [
+                'price_data' => [
+                    'currency' => 'usd',
+                    'product_data' => [
                         'name' => $product->name,
-                        // 'image' => [$product->image]
-                      ],  
-                      'unit_amount' => $product->price * 100,
+                        'images' => [$product->image]
                     ],
-                    'quantity' => 1,      
+                    'unit_amount' => $product->price * 100,
+                ],
+                'quantity' => 1,
             ];
         }
         $session = \Stripe\Checkout\Session::create([
             'line_items' => $lineItems,
             'mode' => 'payment',
-            'success_url' => route('checkout.success', [], true)."?session_id={CHECKOUT_SESSION_ID}",
-            'cancel_url' => route('checkout.cancel', [], true)
-          ]);
+            'success_url' => route('checkout.success', [], true) . "?session_id={CHECKOUT_SESSION_ID}",
+            'cancel_url' => route('checkout.cancel', [], true),
+        ]);
 
-          $order = new Order();
-          $order->status = 'unpaid';
-          $order->total_price = $totalPrice;
-          $order->session_id = $session->id;
-          $order->save();
+        $order = new Order();
+        $order->status = 'unpaid';
+        $order->total_price = $totalPrice;
+        $order->session_id = $session->id;
+        $order->save();
 
         return redirect($session->url);
     }
 
     public function success(Request $request)
     {
-        \Stripe\Stripe::setApiKey('sk_test_51NcJkhFJK6oGjToujz6HzEokn7KwX4SPb2XPytLXjnrTDoZxbrofsOZmcjfgkh1mAnGmSiuLMaJgByYAW1okEgEj00rjXzjmRl');
+        \Stripe\Stripe::setApiKey("sk_test_51NcJkhFJK6oGjToujz6HzEokn7KwX4SPb2XPytLXjnrTDoZxbrofsOZmcjfgkh1mAnGmSiuLMaJgByYAW1okEgEj00rjXzjmRl");
         $sessionId = $request->get('session_id');
 
         try {
@@ -76,7 +76,7 @@ class ProductController extends Controller
             }
 
             return view('product.checkout-success', compact('customer'));
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             throw new NotFoundHttpException();
         }
 
@@ -84,6 +84,47 @@ class ProductController extends Controller
 
     public function cancel()
     {
-        
+
+    }
+
+    public function webhook()
+    {
+        // This is your Stripe CLI webhook secret for testing your endpoint locally.
+        $endpoint_secret = ("whsec_4e2407f0748e1b1b77050897d5ca7da0af7674e88b75f21a73ebe072c1daa8fa");
+
+        $payload = @file_get_contents('php://input');
+        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+        $event = null;
+
+        try {
+            $event = \Stripe\Webhook::constructEvent(
+                $payload, $sig_header, $endpoint_secret
+            );
+        } catch (\UnexpectedValueException $e) {
+            // Invalid payload
+            return response('', 400);
+        } catch (\Stripe\Exception\SignatureVerificationException $e) {
+            // Invalid signature
+            return response('', 400);
+        }
+
+// Handle the event
+        switch ($event->type) {
+            case 'checkout.session.completed':
+                $session = $event->data->object;
+
+                $order = Order::where('session_id', $session->id)->first();
+                if ($order && $order->status === 'unpaid') {
+                    $order->status = 'paid';
+                    $order->save();
+                    // Send email to customer
+                }
+
+            // ... handle other event types
+            default:
+                echo 'Received unknown event type ' . $event->type;
+        }
+
+        return response('');
     }
 }
--- resources/views/product/checkout-success.blade.php
+++ resources/views/product/checkout-success.blade.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-    </head>
-    <body class="antialiased">
-        <h1> SUCCESS! </h1>
-        <p>{{ $customer->name }}</p>
-    </body>
+    <title>Laravel</title>
+</head>
+<body class="antialiased">
+    <h1>Success</h1>
+    <p>{{$customer->name}}</p>
+</body>
 </html>
- 
