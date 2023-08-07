cdf67e74ae2b05adb21dfb66e7eb2c319882ec7a
Edzel <dev.edzeldacanay@gmail.com>
2023-08-07 10:32:37 +0800

    adding stripe

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(Request $request)
+    {
+        $products = Product::all();
+        return view('product.index', compact('products'));
+    }
+
+    public function checkout()
+    {
+        
+    }
+}
